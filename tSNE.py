4afc9ffb90fee01b8c72b0fa375ec8de3a29cef9
xuesen-lab0 <kfsaghak19@gmail.com>
2025-02-25 14:25:07 +0800

    adjustment2

--- tSNE.py
+++ tSNE.py
@@ -1,3 +1,11 @@
+'''
+Author: xuesen-lab0 kfsaghak19@gmail.com
+Date: 2025-02-25 10:46:16
+LastEditors: xuesen-lab0 kfsaghak19@gmail.com
+LastEditTime: 2025-02-25 14:23:04
+FilePath: \winter-quiz\tSNE.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 import numpy as np
 from sklearn.manifold import TSNE
 from sklearn.preprocessing import StandardScaler
@@ -22,9 +30,9 @@ embeddings = scaler.fit_transform(embeddings)
 tsne = TSNE(
     n_components=2, 
     random_state=42, 
-    perplexity=50,  
-    learning_rate=500, 
-    n_iter=3000,  # 迭代次数
+    perplexity=70,  
+    learning_rate=200, 
+    n_iter=1000,  # 迭代次数
     early_exaggeration=12
 )
 
